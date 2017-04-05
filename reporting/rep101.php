808ceb9afaba0adba3a91dfbe8bb5f5218a9ce38
Eugene F. Barker <genebarker@gmail.com>
2017-04-06 11:16:21 +1300

    Make alloc col optional in cust/supp balances rpts
    - add show allocation control, default to No
    - cleanup all sorts of whitespace issues

--- reporting/rep101.php
+++ reporting/rep101.php
@@ -1,13 +1,13 @@
 <?php
 /**********************************************************************
-    Copyright (C) FrontAccounting, LLC.
-	Released under the terms of the GNU General Public License, GPL, 
-	as published by the Free Software Foundation, either version 3 
+	Copyright (C) FrontAccounting, LLC.
+	Released under the terms of the GNU General Public License, GPL,
+	as published by the Free Software Foundation, either version 3
 	of the License, or (at your option) any later version.
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
-    See the License here <http://www.gnu.org/licenses/gpl-3.0.html>.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	See the License here <http://www.gnu.org/licenses/gpl-3.0.html>.
 ***********************************************************************/
 $page_security = 'SA_CUSTPAYMREP';
 
@@ -35,23 +35,23 @@ function get_open_balance($debtorno, $to)
 	if($to)
 		$to = date2sql($to);
 
-    $sql = "SELECT SUM(IF(t.type = ".ST_SALESINVOICE." OR t.type = ".ST_BANKPAYMENT.",
-    	(t.ov_amount + t.ov_gst + t.ov_freight + t.ov_freight_tax + t.ov_discount), 0)) AS charges,
-    	SUM(IF(t.type <> ".ST_SALESINVOICE." AND t.type <> ".ST_BANKPAYMENT.",
-	    	(t.ov_amount + t.ov_gst + t.ov_freight + t.ov_freight_tax + t.ov_discount) * -1, 0)) AS credits,
+	$sql = "SELECT SUM(IF(t.type = ".ST_SALESINVOICE." OR t.type = ".ST_BANKPAYMENT.",
+		(t.ov_amount + t.ov_gst + t.ov_freight + t.ov_freight_tax + t.ov_discount), 0)) AS charges,
+		SUM(IF(t.type <> ".ST_SALESINVOICE." AND t.type <> ".ST_BANKPAYMENT.",
+			(t.ov_amount + t.ov_gst + t.ov_freight + t.ov_freight_tax + t.ov_discount) * -1, 0)) AS credits,
 		SUM(t.alloc) AS Allocated,
 		SUM(IF(t.type = ".ST_SALESINVOICE." OR t.type = ".ST_BANKPAYMENT.",
 			(t.ov_amount + t.ov_gst + t.ov_freight + t.ov_freight_tax + t.ov_discount - t.alloc),
-	    	((t.ov_amount + t.ov_gst + t.ov_freight + t.ov_freight_tax + t.ov_discount) * -1 + t.alloc))) AS OutStanding
+			((t.ov_amount + t.ov_gst + t.ov_freight + t.ov_freight_tax + t.ov_discount) * -1 + t.alloc))) AS OutStanding
 		FROM ".TB_PREF."debtor_trans t
-    	WHERE t.debtor_no = ".db_escape($debtorno)
+		WHERE t.debtor_no = ".db_escape($debtorno)
 		." AND t.type <> ".ST_CUSTDELIVERY;
-    if ($to)
-    	$sql .= " AND t.tran_date < '$to'";
+	if ($to)
+		$sql .= " AND t.tran_date < '$to'";
 	$sql .= " GROUP BY debtor_no";
 
-    $result = db_query($sql,"No transactions were returned");
-    return db_fetch($result);
+	$result = db_query($sql,"No transactions were returned");
+	return db_fetch($result);
 }
 
 function get_transactions($debtorno, $from, $to)
@@ -59,37 +59,38 @@ function get_transactions($debtorno, $from, $to)
 	$from = date2sql($from);
 	$to = date2sql($to);
 
-    $sql = "SELECT ".TB_PREF."debtor_trans.*,
+	$sql = "SELECT ".TB_PREF."debtor_trans.*,
 		(".TB_PREF."debtor_trans.ov_amount + ".TB_PREF."debtor_trans.ov_gst + ".TB_PREF."debtor_trans.ov_freight + 
 		".TB_PREF."debtor_trans.ov_freight_tax + ".TB_PREF."debtor_trans.ov_discount)
 		AS TotalAmount, ".TB_PREF."debtor_trans.alloc AS Allocated,
 		((".TB_PREF."debtor_trans.type = ".ST_SALESINVOICE.")
 		AND ".TB_PREF."debtor_trans.due_date < '$to') AS OverDue
-    	FROM ".TB_PREF."debtor_trans
-    	WHERE ".TB_PREF."debtor_trans.tran_date >= '$from'
+		FROM ".TB_PREF."debtor_trans
+		WHERE ".TB_PREF."debtor_trans.tran_date >= '$from'
 		AND ".TB_PREF."debtor_trans.tran_date <= '$to'
 		AND ".TB_PREF."debtor_trans.debtor_no = ".db_escape($debtorno)."
 		AND ".TB_PREF."debtor_trans.type <> ".ST_CUSTDELIVERY."
-    	ORDER BY ".TB_PREF."debtor_trans.tran_date";
+		ORDER BY ".TB_PREF."debtor_trans.tran_date";
 
-    return db_query($sql,"No transactions were returned");
+	return db_query($sql,"No transactions were returned");
 }
 
 //----------------------------------------------------------------------------------------------------
 
 function print_customer_balances()
 {
-   	global $path_to_root, $systypes_array, $print_invoice_no;
+	global $path_to_root, $systypes_array, $print_invoice_no;
 
-   	$from = $_POST['PARAM_0'];
-   	$to = $_POST['PARAM_1'];
-   	$fromcust = $_POST['PARAM_2'];
-   	$show_balance = $_POST['PARAM_3'] ? false : true;
-   	$currency = $_POST['PARAM_4'];
-   	$no_zeros = $_POST['PARAM_5'];
-   	$comments = $_POST['PARAM_6'];
-	$orientation = $_POST['PARAM_7'];
-	$destination = $_POST['PARAM_8'];
+	$from = $_POST['PARAM_0'];
+	$to = $_POST['PARAM_1'];
+	$fromcust = $_POST['PARAM_2'];
+	$show_allocation = $_POST['PARAM_3'];
+	$show_balance = $_POST['PARAM_4'] ? false : true;
+	$currency = $_POST['PARAM_5'];
+	$no_zeros = $_POST['PARAM_6'];
+	$comments = $_POST['PARAM_7'];
+	$orientation = $_POST['PARAM_8'];
+	$destination = $_POST['PARAM_9'];
 	if ($destination)
 		include_once($path_to_root . "/reporting/includes/excel_report.inc");
 	else
@@ -101,7 +102,7 @@ function print_customer_balances()
 	else
 		$cust = get_customer_name($fromcust);
 
-    $dec = user_price_dec();
+	$dec = user_price_dec();
 
 	if ($currency == ALL_TEXT)
 	{
@@ -117,25 +118,26 @@ function print_customer_balances()
 	// display regular values in balance column as positive numbers
 	$sign = -1;
 
-	$cols = array(0, 100, 130, 190,	250, 320, 385, 450,	515);
+	$cols = array(0, 100, 130, 190, 250, 320, 385, 450, 515);
 
+	$allocation_heading = $show_allocation ? _('Allocated') : _(' ');
 	$headers = array(_('Trans Type'), _('#'), _('Date'), _('Due Date'), _('Charges'), _('Credits'),
-					 _('Allocated'), _('Balance'));
+					 $allocation_heading, _('Balance'));
 
-	$aligns = array('left',	'left',	'left',	'left',	'right', 'right', 'right', 'right');
+	$aligns = array('left', 'left', 'left', 'left', 'right', 'right', 'right', 'right');
 
-    $params =   array( 	0 => $comments,
-    				    1 => array('text' => _('Period'), 'from' => $from, 		'to' => $to),
-    				    2 => array('text' => _('Customer'), 'from' => $cust,   	'to' => ''),
-    				    3 => array('text' => _('Currency'), 'from' => $currency, 'to' => ''),
+	$params =   array( 	0 => $comments,
+						1 => array('text' => _('Period'), 'from' => $from, 'to' => $to),
+						2 => array('text' => _('Customer'), 'from' => $cust, 'to' => ''),
+						3 => array('text' => _('Currency'), 'from' => $currency, 'to' => ''),
 						4 => array('text' => _('Suppress Zeros'), 'from' => $nozeros, 'to' => ''));
 
-    $rep = new FrontReport(_('Customer Balances'), "CustomerBalances", user_pagesize(), 9, $orientation);
-    if ($orientation == 'L')
-    	recalculate_cols($cols);
-    $rep->Font();
-    $rep->Info($params, $cols, $headers, $aligns);
-    $rep->NewPage();
+	$rep = new FrontReport(_('Customer Balances'), "CustomerBalances", user_pagesize(), 9, $orientation);
+	if ($orientation == 'L')
+		recalculate_cols($cols);
+	$rep->Font();
+	$rep->Info($params, $cols, $headers, $aligns);
+	$rep->NewPage();
 
 	$grandtotal = array(0,0,0,0);
 
@@ -160,8 +162,8 @@ function print_customer_balances()
 		{
 			$init[3] = $init[0] - $init[1];
 			$accumulate += $init[3];
-		}	
-		else	
+		}
+		else
 			$init[3] = round2($bal['OutStanding']*$rate, $dec);
 
 		$res = get_transactions($myrow['debtor_no'], $from, $to);
@@ -175,7 +177,8 @@ function print_customer_balances()
 		$rep->TextCol(3, 4,	_("Open Balance"));
 		$rep->AmountCol(4, 5, $init[0], $dec);
 		$rep->AmountCol(5, 6, $init[1], $dec);
-		$rep->AmountCol(6, 7, $init[2], $dec);
+		if ($show_allocation)
+			$rep->AmountCol(6, 7, $init[2], $dec);
 		$rep->AmountCol(7, 8, $sign*$init[3], $dec);
 		$total = array(0,0,0,0);
 		for ($i = 0; $i < 4; $i++)
@@ -214,14 +217,15 @@ function print_customer_balances()
 				$accumulate -= $item[1];
 			}
 			$item[2] = round2($trans['Allocated'] * $rate, $dec);
-			$rep->AmountCol(6, 7, $item[2], $dec);
+			if ($show_allocation)
+				$rep->AmountCol(6, 7, $item[2], $dec);
 			if ($trans['type'] == ST_SALESINVOICE || $trans['type'] == ST_BANKPAYMENT)
 				$item[3] = $item[0] + $item[1] - $item[2];
-			else	
+			else
 				$item[3] = $item[0] - $item[1] + $item[2];
 			if ($show_balance)	
 				$rep->AmountCol(7, 8, $sign*$accumulate, $dec);
-			else	
+			else
 				$rep->AmountCol(7, 8, $sign*$item[3], $dec);
 			for ($i = 0; $i < 4; $i++)
 			{
@@ -234,8 +238,10 @@ function print_customer_balances()
 		$rep->Line($rep->row - 8);
 		$rep->NewLine(2);
 		$rep->TextCol(0, 3, _('Total'));
-		for ($i = 0; $i < 4; $i++)
-			$rep->AmountCol($i + 4, $i + 5, $i==3 ? $sign*$total[$i] : $total[$i], $dec);
+		for ($i = 0; $i < 4; $i++) {
+			if ($i == 2 && !$show_allocation) continue;
+			$rep->AmountCol($i + 4, $i + 5, $i == 3 ? $sign * $total[$i] : $total[$i], $dec);
+		}
    		$rep->Line($rep->row  - 4);
    		$rep->NewLine(2);
 	}
@@ -244,11 +250,13 @@ function print_customer_balances()
 	$rep->fontSize -= 2;
 	if ($show_balance)
 		$grandtotal[3] = $grandtotal[0] - $grandtotal[1];
-	for ($i = 0; $i < 4; $i++)
-		$rep->AmountCol($i + 4, $i + 5, $i==3 ? $sign*$grandtotal[$i] : $grandtotal[$i], $dec);
+	for ($i = 0; $i < 4; $i++) {
+		if ($i == 2 && !$show_allocation) continue;
+		$rep->AmountCol($i + 4, $i + 5, $i == 3 ? $sign * $grandtotal[$i] : $grandtotal[$i], $dec);
+	}
 	$rep->Line($rep->row  - 4);
 	$rep->NewLine();
-    	$rep->End();
+	$rep->End();
 }
 
 ?>
--- reporting/rep201.php
+++ reporting/rep201.php
@@ -1,13 +1,13 @@
 <?php
 /**********************************************************************
-    Copyright (C) FrontAccounting, LLC.
-	Released under the terms of the GNU General Public License, GPL, 
-	as published by the Free Software Foundation, either version 3 
+	Copyright (C) FrontAccounting, LLC.
+	Released under the terms of the GNU General Public License, GPL,
+	as published by the Free Software Foundation, either version 3
 	of the License, or (at your option) any later version.
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
-    See the License here <http://www.gnu.org/licenses/gpl-3.0.html>.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	See the License here <http://www.gnu.org/licenses/gpl-3.0.html>.
 ***********************************************************************/
 $page_security = 'SA_SUPPLIERANALYTIC';
 // ----------------------------------------------------------------
@@ -31,20 +31,20 @@ function get_open_balance($supplier_id, $to)
 {
 	$to = date2sql($to);
 
-    $sql = "SELECT SUM(IF(".TB_PREF."supp_trans.type = ".ST_SUPPINVOICE." OR ".TB_PREF."supp_trans.type = ".ST_BANKDEPOSIT.", 
-    	(".TB_PREF."supp_trans.ov_amount + ".TB_PREF."supp_trans.ov_gst + ".TB_PREF."supp_trans.ov_discount), 0)) AS charges,
-    	SUM(IF(".TB_PREF."supp_trans.type <> ".ST_SUPPINVOICE." AND ".TB_PREF."supp_trans.type <> ".ST_BANKDEPOSIT.", 
-    	(".TB_PREF."supp_trans.ov_amount + ".TB_PREF."supp_trans.ov_gst + ".TB_PREF."supp_trans.ov_discount), 0)) AS credits,
+	$sql = "SELECT SUM(IF(".TB_PREF."supp_trans.type = ".ST_SUPPINVOICE." OR ".TB_PREF."supp_trans.type = ".ST_BANKDEPOSIT.", 
+		(".TB_PREF."supp_trans.ov_amount + ".TB_PREF."supp_trans.ov_gst + ".TB_PREF."supp_trans.ov_discount), 0)) AS charges,
+		SUM(IF(".TB_PREF."supp_trans.type <> ".ST_SUPPINVOICE." AND ".TB_PREF."supp_trans.type <> ".ST_BANKDEPOSIT.", 
+		(".TB_PREF."supp_trans.ov_amount + ".TB_PREF."supp_trans.ov_gst + ".TB_PREF."supp_trans.ov_discount), 0)) AS credits,
 		SUM(".TB_PREF."supp_trans.alloc) AS Allocated,
 		SUM(IF(".TB_PREF."supp_trans.type = ".ST_SUPPINVOICE." OR ".TB_PREF."supp_trans.type = ".ST_BANKDEPOSIT.",
 		(".TB_PREF."supp_trans.ov_amount + ".TB_PREF."supp_trans.ov_gst + ".TB_PREF."supp_trans.ov_discount - ".TB_PREF."supp_trans.alloc),
 		(".TB_PREF."supp_trans.ov_amount + ".TB_PREF."supp_trans.ov_gst + ".TB_PREF."supp_trans.ov_discount + ".TB_PREF."supp_trans.alloc))) AS OutStanding
 		FROM ".TB_PREF."supp_trans
-    	WHERE ".TB_PREF."supp_trans.tran_date < '$to'
+		WHERE ".TB_PREF."supp_trans.tran_date < '$to'
 		AND ".TB_PREF."supp_trans.supplier_id = '$supplier_id' GROUP BY supplier_id";
 
-    $result = db_query($sql,"No transactions were returned");
-    return db_fetch($result);
+	$result = db_query($sql,"No transactions were returned");
+	return db_fetch($result);
 }
 
 function getTransactions($supplier_id, $from, $to)
@@ -52,19 +52,19 @@ function getTransactions($supplier_id, $from, $to)
 	$from = date2sql($from);
 	$to = date2sql($to);
 
-    $sql = "SELECT ".TB_PREF."supp_trans.*,
+	$sql = "SELECT ".TB_PREF."supp_trans.*,
 				(".TB_PREF."supp_trans.ov_amount + ".TB_PREF."supp_trans.ov_gst + ".TB_PREF."supp_trans.ov_discount)
 				AS TotalAmount, ".TB_PREF."supp_trans.alloc AS Allocated,
 				((".TB_PREF."supp_trans.type = ".ST_SUPPINVOICE.")
 					AND ".TB_PREF."supp_trans.due_date < '$to') AS OverDue
-    			FROM ".TB_PREF."supp_trans
-    			WHERE ".TB_PREF."supp_trans.tran_date >= '$from' AND ".TB_PREF."supp_trans.tran_date <= '$to' 
-    			AND ".TB_PREF."supp_trans.supplier_id = '$supplier_id' AND ".TB_PREF."supp_trans.ov_amount!=0
-    				ORDER BY ".TB_PREF."supp_trans.tran_date";
+				FROM ".TB_PREF."supp_trans
+				WHERE ".TB_PREF."supp_trans.tran_date >= '$from' AND ".TB_PREF."supp_trans.tran_date <= '$to' 
+				AND ".TB_PREF."supp_trans.supplier_id = '$supplier_id' AND ".TB_PREF."supp_trans.ov_amount!=0
+					ORDER BY ".TB_PREF."supp_trans.tran_date";
 
-    $TransResult = db_query($sql,"No transactions were returned");
+	$TransResult = db_query($sql,"No transactions were returned");
 
-    return $TransResult;
+	return $TransResult;
 }
 
 //----------------------------------------------------------------------------------------------------
@@ -76,12 +76,13 @@ function print_supplier_balances()
 	$from = $_POST['PARAM_0'];
 	$to = $_POST['PARAM_1'];
 	$fromsupp = $_POST['PARAM_2'];
-	$show_balance = $_POST['PARAM_3'] ? false : true;
-	$currency = $_POST['PARAM_4'];
-	$no_zeros = $_POST['PARAM_5'];
-	$comments = $_POST['PARAM_6'];
-	$orientation = $_POST['PARAM_7'];
-	$destination = $_POST['PARAM_8'];
+	$show_allocation = $_POST['PARAM_3'];
+	$show_balance = $_POST['PARAM_4'] ? false : true;
+	$currency = $_POST['PARAM_5'];
+	$no_zeros = $_POST['PARAM_6'];
+	$comments = $_POST['PARAM_7'];
+	$orientation = $_POST['PARAM_8'];
+	$destination = $_POST['PARAM_9'];
 	if ($destination)
 		include_once($path_to_root . "/reporting/includes/excel_report.inc");
 	else
@@ -92,7 +93,8 @@ function print_supplier_balances()
 		$supp = _('All');
 	else
 		$supp = get_supplier_name($fromsupp);
-    	$dec = user_price_dec();
+
+	$dec = user_price_dec();
 
 	if ($currency == ALL_TEXT)
 	{
@@ -105,31 +107,31 @@ function print_supplier_balances()
 	if ($no_zeros) $nozeros = _('Yes');
 	else $nozeros = _('No');
 
-    // display regular values in balance column as positive numbers
-    $sign = -1;
+	// display regular values in balance column as positive numbers
+	$sign = -1;
 
-	$cols = array(0, 100, 130, 190,	250, 320, 385, 450,	515);
+	$cols = array(0, 100, 130, 190, 250, 320, 385, 450, 515);
 
+	$allocation_heading = $show_allocation ? _('Allocated') : _(' ');
+	$balance_heading = $show_balance ? _('Balance') : _('Outstanding');
 	$headers = array(_('Trans Type'), _('#'), _('Date'), _('Due Date'), _('Charges'),
-		_('Credits'), _('Allocated'), _('Outstanding'));
+		_('Credits'), $allocation_heading, $balance_heading);
 
-	if ($show_balance)
-		$headers[7] = _('Balance');
 	$aligns = array('left',	'left',	'left',	'left',	'right', 'right', 'right', 'right');
 
-    $params =   array( 	0 => $comments,
-    			1 => array('text' => _('Period'), 'from' => $from, 'to' => $to),
-    			2 => array('text' => _('Supplier'), 'from' => $supp, 'to' => ''),
-    			3 => array(  'text' => _('Currency'),'from' => $currency, 'to' => ''),
-			4 => array('text' => _('Suppress Zeros'), 'from' => $nozeros, 'to' => ''));
+	$params = array(0 => $comments,
+				1 => array('text' => _('Period'), 'from' => $from, 'to' => $to),
+				2 => array('text' => _('Supplier'), 'from' => $supp, 'to' => ''),
+				3 => array('text' => _('Currency'),'from' => $currency, 'to' => ''),
+				4 => array('text' => _('Suppress Zeros'), 'from' => $nozeros, 'to' => ''));
 
-    $rep = new FrontReport(_('Supplier Balances'), "SupplierBalances", user_pagesize(), 9, $orientation);
-    if ($orientation == 'L')
-    	recalculate_cols($cols);
+	$rep = new FrontReport(_('Supplier Balances'), "SupplierBalances", user_pagesize(), 9, $orientation);
+	if ($orientation == 'L')
+		recalculate_cols($cols);
 
-    $rep->Font();
-    $rep->Info($params, $cols, $headers, $aligns);
-    $rep->NewPage();
+	$rep->Font();
+	$rep->Info($params, $cols, $headers, $aligns);
+	$rep->NewPage();
 
 	$total = array();
 	$grandtotal = array(0,0,0,0);
@@ -168,7 +170,8 @@ function print_supplier_balances()
 		$rep->TextCol(3, 4,	_("Open Balance"));
 		$rep->AmountCol(4, 5, $init[0], $dec);
 		$rep->AmountCol(5, 6, $init[1], $dec);
-		$rep->AmountCol(6, 7, $init[2], $dec);
+		if ($show_allocation)
+			$rep->AmountCol(6, 7, $init[2], $dec);
 		$rep->AmountCol(7, 8, $sign*$init[3], $dec);
 		$total = array(0,0,0,0);
 		for ($i = 0; $i < 4; $i++)
@@ -205,7 +208,8 @@ function print_supplier_balances()
 				$accumulate -= $item[1];
 			}
 			$item[2] = round2($trans['Allocated'] * $rate, $dec);
-			$rep->AmountCol(6, 7, $item[2], $dec);
+			if ($show_allocation)
+				$rep->AmountCol(6, 7, $item[2], $dec);
 			if ($trans['TotalAmount'] > 0.0)
 				$item[3] = $item[0] - $item[2];
 			else	
@@ -227,22 +231,24 @@ function print_supplier_balances()
 		$rep->TextCol(0, 3,	_('Total'));
 		for ($i = 0; $i < 4; $i++)
 		{
+			if ($i == 2 && !$show_allocation) continue;
 			$rep->AmountCol($i + 4, $i + 5, $i==3 ? $sign*$total[$i] : $total[$i], $dec);
-			$total[$i] = 0.0;
 		}
-    	$rep->Line($rep->row  - 4);
-    	$rep->NewLine(2);
+		$rep->Line($rep->row  - 4);
+		$rep->NewLine(2);
 	}
 	$rep->fontSize += 2;
 	$rep->TextCol(0, 3,	_('Grand Total'));
 	$rep->fontSize -= 2;
 	if ($show_balance)
 		$grandtotal[3] = $grandtotal[0] - $grandtotal[1];
-	for ($i = 0; $i < 4; $i++)
-		$rep->AmountCol($i + 4, $i + 5, $i==3 ? $sign*$grandtotal[$i] : $grandtotal[$i], $dec);
+	for ($i = 0; $i < 4; $i++) {
+		if ($i == 2 && !$show_allocation) continue;
+		$rep->AmountCol($i + 4, $i + 5, $i == 3 ? $sign * $grandtotal[$i] : $grandtotal[$i], $dec);
+	}
 	$rep->Line($rep->row  - 4);
 	$rep->NewLine();
-    $rep->End();
+	$rep->End();
 }
 
 ?>
